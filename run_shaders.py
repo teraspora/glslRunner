aa1a6c474295a296582ae11b591e103c55b4e43d
John Lynch <johnlynch.32768@gmail.com>
2019-08-04 03:09:35 +0100

    Add more shortcuts

--- run_shaders.py
+++ run_shaders.py
@@ -7,34 +7,29 @@
 # from the `imgs` directory, and run an instance of glslViewer
 # with these objects as parameters. 
 
-import os, fnmatch, random
+import os, fnmatch, random, threading
 from subprocess import Popen
 
 SHADER_DIR = '/media/john/sys2/web18/playground/visuals/frag/production'
 IMAGE_DIR = '/media/john/sys2/web18/playground/visuals/imgs/'
-SHADER_CODES = {'b': 'blobacity', 'v': 'voronoi-05', 'c': 'cushion-x1', 'g': 'cogs-new00',
-                'G': 'cogs-x3', 'd': 'deep-sea-x1', 'e': 'deep-sea-x', 'D': 'dance00', 't': 'trig-dreamz-03',
-                'O': 'trig-dreamz01'}
+SHADER_CODES = {'b': 'blobacity', 'v': 'voronoi-05', 'c': 'cushion-x1', 'C': 'cushion-x2', '¢': 'cushion-x4', 'g': 'cogs-new00',
+                'G': 'cogs-x3', 'd': 'deep-sea-x1', 'e': 'deep-sea-x', 'D': 'dance00', 'f': 'feb-18-03', 'F': 'feb-test',
+                'O': 'trig-dreamz01', 'o': 'trig-dreamz-01', 't': 'trig-dreamz-03', 'T': 'trig-dreamz-05', 'a': 'anon091', 'A': 'anon10'}
 EXTENSION = '.frag'
 CMD_REDIRECT = ['>', '/dev/null', '2>&1']
-
-def get_files(pattern, basedir):
-    for path, dirs, files in os.walk(basedir):
-        for fname in fnmatch.filter(files, pattern):
-            yield os.path.join(path, fname)
-
 
 def choose_files(path, number):
     files = os.listdir(path)
     chosenfiles = random.choices(files, k = number)
     return chosenfiles
 
-prod_shaders = fnmatch.filter(os.listdir(SHADER_DIR), '*.frag')
+shaders = fnmatch.filter(os.listdir(SHADER_DIR), '*.frag')
 
 # Run glslViewer from Python:
 def run(shader):
+    print(shader)
     imgs = [f'{os.path.join(IMAGE_DIR, img)}' for img in choose_files(IMAGE_DIR, 8)]
-    glsl_cmd = ['glslViewer', f'{SHADER_DIR}/{shader}'] + imgs + CMD_REDIRECT
+    glsl_cmd = ['glslViewer -w 1920 -h 1080', f'{SHADER_DIR}/{shader}'] + imgs + CMD_REDIRECT
     cmd_str = ' '.join(glsl_cmd)
     Popen(cmd_str, shell=True)
     return
@@ -42,5 +37,7 @@ def run(shader):
 if __name__ == '__main__':
     while True:
         command = input('Run shader?') or ' '
-        shader = SHADER_CODES[command[0]] + EXTENSION if command[0] in SHADER_CODES else random.choice(prod_shaders)
-        run(shader)
+        shader = SHADER_CODES[command[0]] + EXTENSION if command[0] in SHADER_CODES else random.choice(shaders)
+        # Spawn a thread to run the shader
+        runner = threading.Thread(target=run, args=(shader,), name='runner')
+        runner.start()
